70694b900c2726bb44df3397fbac78e1cb9e7071
Ryan McLoughlin <95232745+SaucyPantsu@users.noreply.github.com>
2023-03-30 13:10:23 +0100

    Update Naive.java

--- Assignment/src/assignmentcode/Naive.java
+++ Assignment/src/assignmentcode/Naive.java
@@ -1,12 +1,14 @@
 package assignmentcode;
 
 import java.io.*;
-import java.util.*;
+import java.math.*;
 
 public class Naive {
 	//dataset location
 	String dataset;
 	String[] column;
+	int TestSize;
+	int TrainSize;
 	//total number of subjects
 	int linecount;
 	double ifEntrepreneur;
@@ -41,10 +43,53 @@ public class Naive {
 	double probext;
 	double test;
 	double notEntrepreneur;
+	int count;
+	
 	
 	//configuring naive to use default dataset or a custom one
 	public Naive(String dataset) {
 		this.dataset = dataset;
+	}
+	
+	public void doMyMath() {
+		
+		//collating general stats
+				ggender = gender+Egender;
+				gParent = Parent+EParent;
+				gjob = ejob+ job;
+				gArea = Area+EArea;
+				gbusinessStudies = businessStudies+EbusinessStudies;
+				
+				//general probabilities
+				gpgender = (double)ggender/linecount;
+				gpParent = (double)gParent/linecount;
+				gpjob = (double)gjob/linecount;
+				gpArea =(double) gArea/linecount;
+				gpbusinessStudies = (double)gbusinessStudies/linecount;
+				gpentrepreneur = (double)Eentrepreneur/linecount;
+				
+				//obtaining entrepreneur probabilities
+				epgender = (double)Egender/Eentrepreneur;
+				epParent =(double) EParent/Eentrepreneur;
+				epJob =(double) ejob/Eentrepreneur;
+				epArea = (double)EArea/Eentrepreneur;
+				epbusinessStudies =(double) EbusinessStudies/Eentrepreneur;
+				
+				//obtaining not entrepreneur probabilities
+				rpgender = (double)gender/Nentrepreneur;
+				rpParent = (double)Parent/Nentrepreneur;
+				rpJob =(double) job/Nentrepreneur;
+				rpArea =(double) Area/Nentrepreneur;
+				rpbusinessStudies = (double)businessStudies/Nentrepreneur;
+				
+				//naive bayes calculation
+				
+				probEnt = (double)Eentrepreneur/linecount;
+				notEntrepreneur = 1-probEnt;
+				
+				ifEntrepreneur = epgender*epParent*epJob*epbusinessStudies*epArea*probEnt;
+				test = rpgender*rpParent*rpJob*rpArea*rpbusinessStudies*notEntrepreneur;
+				//
 	}
 	
 	public void runGeneralNaive() throws FileNotFoundException {
@@ -112,45 +157,13 @@ public class Naive {
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-		//collating general stats
-		ggender = gender+Egender;
-		gParent = Parent+EParent;
-		gjob = ejob+ job;
-		gArea = Area+EArea;
-		gbusinessStudies = businessStudies+EbusinessStudies;
-		
-		//general probabilities
-		gpgender = (double)ggender/linecount;
-		gpParent = (double)gParent/linecount;
-		gpjob = (double)gjob/linecount;
-		gpArea =(double) gArea/linecount;
-		gpbusinessStudies = (double)gbusinessStudies/linecount;
-		gpentrepreneur = (double)Eentrepreneur/linecount;
-		
-		//obtaining entrepreneur probabilities
-		epgender = (double)Egender/Eentrepreneur;
-		epParent =(double) EParent/Eentrepreneur;
-		epJob =(double) ejob/Eentrepreneur;
-		epArea = (double)EArea/Eentrepreneur;
-		epbusinessStudies =(double) EbusinessStudies/Eentrepreneur;
-		
-		//obtaining not entrepreneur probabilities
-		rpgender = (double)gender/Nentrepreneur;
-		rpParent = (double)Parent/Nentrepreneur;
-		rpJob =(double) job/Nentrepreneur;
-		rpArea =(double) Area/Nentrepreneur;
-		rpbusinessStudies = (double)businessStudies/Nentrepreneur;
-		
-		//naive bayes calculation
-		
-		probEnt = (double)Eentrepreneur/linecount;
-		notEntrepreneur = 1-probEnt;
-		
-		ifEntrepreneur = epgender*epParent*epJob*epbusinessStudies*epArea*probEnt;
-		test = rpgender*rpParent*rpJob*rpArea*rpbusinessStudies*notEntrepreneur;
-		//
-		
+		doMyMath();
+		runTest();
 		//System.out.println(probEnt);
+		//testing data
+		
+	}
+	public void runTest(){
 		probabilities("Male", "No", "No", "Yes", "Urban");
 		System.out.println(ifEntrepreneur);
 		System.out.println(probext);
@@ -162,8 +175,86 @@ public class Naive {
 			System.out.println("This person is likely to be an entrepreneur");
 		}
 	}
-	
-	
+
+	public void SelfTest() throws FileNotFoundException{
+		BufferedReader fileread = new BufferedReader(new FileReader(dataset));
+		try {
+			while((dataset = fileread.readLine()) != null) {
+				
+				column = dataset.split(",");
+
+				if(column[0].equals("Female") || column[0].equals("Male"))
+				{
+					linecount = linecount+1;				}
+			}
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		TrainSize = (int) Math.floor((linecount/100)*70);
+		TestSize = linecount - TrainSize;
+		
+		try {
+			for(count =0; count != TrainSize; count++){
+				fileread.readLine();
+				if(column[5].equals("Yes")) {
+					Eentrepreneur = Eentrepreneur + 1;
+					
+					if(column[0].equals("Male")) {
+						Egender = Egender+1;
+					}
+					
+					if(column[1].equals("Yes")) {
+						EParent = EParent +1;
+					}
+					
+					if(column[2].equals("Yes")) {
+						ejob = ejob +1;
+					}
+					
+					if(column[3].equals("Rural")) {
+						EArea = EArea +1;
+					}
+					
+					if(column[4].equals("Yes")) {
+						EbusinessStudies = EbusinessStudies +1;
+					}
+					
+				}
+				//Not entrepreneur stats
+				else if(column[5].equals("No")) {
+					Nentrepreneur = Nentrepreneur + 1;
+					if(column[0].equals("Male")) {
+						gender = gender+1;
+					}
+					
+					if(column[1].equals("Yes")) {
+						Parent = Parent +1;
+					}
+					
+					if(column[2].equals("Yes")) {
+						job = job +1;
+					}
+					
+					if(column[3].equals("Rural")) {
+						Area = Area +1;
+					}
+					
+					if(column[4].equals("Yes")) {
+						businessStudies = businessStudies +1;
+					}
+					
+				}
+				doMyMath();
+				
+			}
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public void probabilities(String gender, String parent, String job, String business, String Area){
 		
 		if(gender.equals("Male"))
