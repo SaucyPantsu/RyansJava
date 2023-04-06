a6a1aa292318fb9a05daff71a59ff3392801c844
Ryan McLoughlin <95232745+SaucyPantsu@users.noreply.github.com>
2023-04-06 13:58:57 +0100

    Control done

--- Assignment/src/assignmentcode/Control.java
+++ Assignment/src/assignmentcode/Control.java
@@ -6,11 +6,7 @@ public class Control {
 	
 
 	public static void main(String[] args) throws FileNotFoundException {
-		Naive data =  new Naive("C:/Users/ryanm/Desktop/Java/RyansJava/Assignment/src/assignmentcode/MLdata.csv");
-				//GUIcontrol screen = new GUIcontrol();
-				//screen.GUI();
-		data.runGeneralNaive();
-
+				GUIcontrol screen = new GUIcontrol();
 	}
 }
 
