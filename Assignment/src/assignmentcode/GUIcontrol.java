56969f376abc8ec7aeb594bb44b71ac38a2e9545
Ryan McLoughlin <95232745+SaucyPantsu@users.noreply.github.com>
2023-04-06 13:58:10 +0100

    gui done

--- Assignment/src/assignmentcode/GUIcontrol.java
+++ Assignment/src/assignmentcode/GUIcontrol.java
@@ -2,144 +2,268 @@ package assignmentcode;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.FileNotFoundException;
+
 import javax.swing.*;
+import net.miginfocom.swing.MigLayout;
 
 
 public class GUIcontrol extends JFrame implements ActionListener, MouseListener{
-	JFrame Frame;
-	JLabel Label, Label2, Label3, Label4;
-	JButton Button, Button2, Button3, Button4;
-	JTextField Text;
-	JPanel Panel, Panel2, Panel3, Panel4;
-	String dataLocation;
+	JFrame f;
+	double generalresult;
+	double accuracy;
+	String Gender = "Female";
+	String Area = "Rural";
+	String Parents = "No";
+	String Job = "No";
+	String Business = "No";
+	boolean Test;
+	public GUIcontrol() {
+		Naive data = new Naive("MLdata.csv");
+		setTitle("Naive Bayes Classifier");
+		setSize(500,300);
+		
+		 GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{130, 130,130, 0};
+		gridBagLayout.rowHeights = new int[]{14, 38, 23, 23, 23, 23, 23, 23, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		getContentPane().setLayout(gridBagLayout);
+		
+		JLabel lblNewLabel_1 = new JLabel("Run Naive Bayes");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 0;
+		getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		
+		JLabel lblNewLabel = new JLabel("Run Self Test");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 0;
+		getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Test Custom Data");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_2.gridx = 2;
+		gbc_lblNewLabel_2.gridy = 0;
+		getContentPane().add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		JButton btnNewButton = new JButton("Run Naive Bayes");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					try {
+						data.runGeneralNaive();
+						generalresult = data.results2;
+					}
+					catch(NullPointerException e1) {
+						generalresult = data.results2;
+					}
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				JOptionPane.showMessageDialog(null, "Probability of entrepreneur: "+ generalresult);
+			}
+		});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 2;
+		getContentPane().add(btnNewButton, gbc_btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Run Self Test");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					try {
+						data.SelfTest();
+						accuracy = data.accuracy;
+					}
+					catch(NullPointerException e1) {
+
+						accuracy = data.accuracy;
+					}
+					
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				JOptionPane.showMessageDialog(null, "Accuracy percentage: "+ accuracy);
+			}
+		});
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_1.gridx = 1;
+		gbc_btnNewButton_1.gridy = 2;
+		getContentPane().add(btnNewButton_1, gbc_btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Test Custom Data");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					try {
+						data.runGeneralNaive();
+						Test = data.runTest(Gender, Parents, Job, Business, Area);
+					}
+					catch(NullPointerException e1) {
+						Test = data.runTest(Gender, Parents, Job, Business, Area);
+					}
+					
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				if(Test == true){
+					JOptionPane.showMessageDialog(null, "This individual is likely to be an entrepreneur");
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "This individual is not likely to be an entrepreneur");
+				}
+			}
+		});
+		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
+		gbc_btnNewButton_2.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton_2.gridx = 2;
+		gbc_btnNewButton_2.gridy = 2;
+		getContentPane().add(btnNewButton_2, gbc_btnNewButton_2);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Click if male");
+		rdbtnNewRadioButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(rdbtnNewRadioButton_1.isSelected()) {
+					Gender = "Male";
+				}
+				else {
+					Gender = "Female";
+				}
+			}
+		});
+		GridBagConstraints gbc_rdbtnNewRadioButton_1 = new GridBagConstraints();
+		gbc_rdbtnNewRadioButton_1.anchor = GridBagConstraints.NORTHWEST;
+		gbc_rdbtnNewRadioButton_1.insets = new Insets(0, 0, 5, 0);
+		gbc_rdbtnNewRadioButton_1.gridx = 2;
+		gbc_rdbtnNewRadioButton_1.gridy = 3;
+		getContentPane().add(rdbtnNewRadioButton_1, gbc_rdbtnNewRadioButton_1);
+		
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Click if Parents are entreprenuers");
+		rdbtnNewRadioButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(rdbtnNewRadioButton_2.isSelected()) {
+					Gender = "Yes";
+				}
+				else {
+					Gender = "No";
+				}
+			}
+		});
+		GridBagConstraints gbc_rdbtnNewRadioButton_2 = new GridBagConstraints();
+		gbc_rdbtnNewRadioButton_2.anchor = GridBagConstraints.NORTHWEST;
+		gbc_rdbtnNewRadioButton_2.insets = new Insets(0, 0, 5, 0);
+		gbc_rdbtnNewRadioButton_2.gridx = 2;
+		gbc_rdbtnNewRadioButton_2.gridy = 4;
+		getContentPane().add(rdbtnNewRadioButton_2, gbc_rdbtnNewRadioButton_2);
+		
+		JRadioButton rdbtnNewRadioButton_4 = new JRadioButton("Click if you studied business");
+		rdbtnNewRadioButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(rdbtnNewRadioButton_4.isSelected()) {
+					Gender = "Yes";
+				}
+				else {
+					Gender = "No";
+				}
+			}
+		});
+		GridBagConstraints gbc_rdbtnNewRadioButton_4 = new GridBagConstraints();
+		gbc_rdbtnNewRadioButton_4.anchor = GridBagConstraints.NORTHWEST;
+		gbc_rdbtnNewRadioButton_4.insets = new Insets(0, 0, 5, 0);
+		gbc_rdbtnNewRadioButton_4.gridx = 2;
+		gbc_rdbtnNewRadioButton_4.gridy = 5;
+		getContentPane().add(rdbtnNewRadioButton_4, gbc_rdbtnNewRadioButton_4);
+		
+		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("Click if you have a part time job");
+		rdbtnNewRadioButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(rdbtnNewRadioButton_3.isSelected()) {
+					Gender = "Yes";
+				}
+				else {
+					Gender = "No";
+				}
+			}
+		});
+		GridBagConstraints gbc_rdbtnNewRadioButton_3 = new GridBagConstraints();
+		gbc_rdbtnNewRadioButton_3.anchor = GridBagConstraints.NORTHWEST;
+		gbc_rdbtnNewRadioButton_3.insets = new Insets(0, 0, 5, 0);
+		gbc_rdbtnNewRadioButton_3.gridx = 2;
+		gbc_rdbtnNewRadioButton_3.gridy = 6;
+		getContentPane().add(rdbtnNewRadioButton_3, gbc_rdbtnNewRadioButton_3);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Click if you live in an Urban area");
+		rdbtnNewRadioButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(rdbtnNewRadioButton.isSelected()) {
+					Gender = "Urban";
+				}
+				else {
+					Gender = "Rural";
+				}
+			}
+		});
+		GridBagConstraints gbc_rdbtnNewRadioButton = new GridBagConstraints();
+		gbc_rdbtnNewRadioButton.anchor = GridBagConstraints.NORTHWEST;
+		gbc_rdbtnNewRadioButton.gridx = 2;
+		gbc_rdbtnNewRadioButton.gridy = 7;
+		getContentPane().add(rdbtnNewRadioButton, gbc_rdbtnNewRadioButton);
+		setVisible(true);
+	}
 	public void GUI() {
-		//defining frame
-		Frame = new JFrame("Naive Bayes Classifier")	;
-		Frame.setSize(900, 600);
-		
-		Frame.setLayout(new GridLayout());
-		
-		//defining labels
-		Label = new JLabel();
-		Label2 = new JLabel();
-		Label3 = new JLabel();
-		
-		//defining buttons
-		Button = new JButton("Train Naive Bayes");
-		Button2 = new JButton("Test your own information");
-		Button3 = new JButton("View current statistics");
-		Button4 = new JButton("Input dataset location");
-		
-		//defining text field
-		Text = new JTextField();
-		
-		//define button Panel
-		Panel = new JPanel();
-		Panel.setLayout(new FlowLayout());
-		Panel.add(Button);
-		Button.setToolTipText("This is Button");
-		Panel.add(Button2);
-		Button2.setToolTipText("This is Button2");
-		Panel.add(Button3);
-		Button3.setToolTipText("This is Button3");
-		
-		//setting up panel 2
-		Panel2 = new JPanel();
-		Panel2.setLayout(new FlowLayout());
-		Panel2.add(Label);
-		Label.setText("I am a label");
-		
-		//setting up panel 3
-		Panel3 = new JPanel();
-		Panel3.setLayout(new FlowLayout());
-		Panel.add(Text);
-		Text.setText("Name");
-		
-		//panel 4 setup
-		Panel4 = new JPanel();
-		Panel4.add(Label4);
-		Panel4.setBackground(Color.blue);
-		
-		//adding listeners
-		Button.addActionListener(this);
-		Button2.addActionListener(this);
-		Button3.addActionListener(this);
-		Panel4.addMouseListener(this);
-		Text.addActionListener(this);
-		
-		
-		//frame setup
-		
-		Frame.add(Panel);
-		Frame.add(Panel2);
-		Frame.add(Panel3);
-		Frame.add(Panel4);
-		Frame.setVisible(true);
-		
-		}
-	
+		GUIcontrol screen = new GUIcontrol();
+	}
+
+
 	@Override
-	
-	//action controller
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==Button)
+		// TODO Auto-generated method stub
 		{
-			JOptionPane.showMessageDialog(this, "You Pressed the Button");
-		}
-		else if(e.getSource()==Button2)
-		{
-			JOptionPane.showMessageDialog(this, "You Pressed the Button2");
-		}
-		else if(e.getSource()==Button3)
-		{
-			JOptionPane.showMessageDialog(this, "You Pressed the Button3");
-		}
-		else if(e.getSource()==Text)
-		{
-			String input = Text.getText();
-			JOptionPane.showMessageDialog(this, "You entered:" + input);
+
 		}
 		
 	}
-
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		//not used
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		if(e.getButton()==MouseEvent.BUTTON1) {
-			JOptionPane.showMessageDialog(this, "You pressed The Left button");
-		}
-		else if(e.getButton()==MouseEvent.BUTTON3) {
-			JOptionPane.showMessageDialog(this, "You pressed The Right Button");
-		}
+		// TODO Auto-generated method stub
 		
 	}
-
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		if(e.getSource()==Panel4) {
-			Label4.setText("The Mouse has Entered");
-			Panel4.setBackground(Color.RED);
-		}
+		// TODO Auto-generated method stub
+		
 	}
-
 	@Override
 	public void mouseExited(MouseEvent e) {
-		if(e.getSource()==Panel4) {
-			Label4.setText("The Mouse has left");
-			Panel4.setBackground(Color.BLUE);
-		}
-		
+		// TODO Auto-generated method stub
 		
 	}
-
-}
+	}
